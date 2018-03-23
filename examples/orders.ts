fc242ecdd389dc644d4edbb192d84ead6517d971
Jaroslav Svak <jaroslavsvak@gmail.com>
2018-03-24 00:25:42 +0100

    initial 11

--- examples/orders.ts
+++ examples/orders.ts
@@ -4,22 +4,22 @@ const orders = [
     {
         customer: 'Cust 1',
         rows: [
-            { item: 'Pancake', qty: 1, pricePerUnit: 1.5 },
-            { item: 'Apple Pie', qty: 5, pricePerUnit: 2 },
-            { item: 'Coke', qty: 3, pricePerUnit: 2.5 }
+            { article: 'Pancake', qty: 1, pricePerUnit: 1.5 },
+            { article: 'Apple Pie', qty: 5, pricePerUnit: 2 },
+            { article: 'Coke', qty: 3, pricePerUnit: 2.5 }
         ]
     },
     {
         customer: 'Cust 1',
         rows: [
-            { item: 'Bread', qty: 7, pricePerUnit: 2 },
-            { item: 'Coke', qty: 1, pricePerUnit: 2.5 },
+            { article: 'Bread', qty: 7, pricePerUnit: 2 },
+            { article: 'Coke', qty: 1, pricePerUnit: 2.5 },
         ]
     },
     {
         customer: 'Cust 2',
         rows: [
-            { item: 'Coke', qty: 2, pricePerUnit: 2.5 }
+            { article: 'Coke', qty: 2, pricePerUnit: 2.5 }
         ]
     }
 ];
@@ -32,22 +32,25 @@ const salesTotal = iter(orders)
 console.log('Total sales', salesTotal);
 
 // sum sales per customer
-const ordersByCust = iter(orders).toMap(o => o.customer);
-
-iter(ordersByCust.keys())
-    .map(custName => {
-        const orders = ordersByCust.get(custName)!;
-        return {
-            customer: custName,
-            sales: iter(orders).sum(o => iter(o.rows).sum(r => r.qty * r.pricePerUnit)!)
-        };
-    })
-    .forEach(c => console.log(c.customer, c.sales));
-
-// sum sales per customer 2nd option
 console.log('Sales per customer');
 
 iter(orders)
     .groupBy(o => o.customer)
-    .map(g => [g.key, g.items.sum(item => iter(item.rows).sum(r => r.pricePerUnit * r.qty))])
+    .map(g => [
+        g.key,
+        g.items.flatMap(o => o.rows).sum(r => r.pricePerUnit * r.qty)
+    ])
     .forEach(c => console.log(c[0], c[1]));
+
+// sales per item
+iter(orders)
+    .flatMap(o => o.rows)
+    .groupBy(r => r.article)
+    .map(i => {
+        return {
+            article:      i.key,
+            totalQtySold: i.items.sum(a => a.qty),
+            totalSales:   i.items.sum(a => a.qty * a.pricePerUnit)
+        }
+    })
+    .forEach(x => console.log(x));
