5ab651cc8a900a2e2c0e7b79216e4051c014be0b
Jaroslav Svak <jaroslavsvak@gmail.com>
2018-04-14 13:33:11 +0200

    min, max, sum improvement

--- fp/math.ts
+++ fp/math.ts
@@ -1,6 +1,11 @@
-export function min<T>(mapper: (item: T) => number): (source: IterableIterator<T>) => number | undefined {
-    return (source: IterableIterator<T>) => {
+export function min<T>(mapper: (item: T) => number): (source: IterableIterator<T>) => number | undefined;
+
+export function min(): (source: IterableIterator<number>) => number | undefined;
+
+export function min(mapper?: (item: any) => number): (source: IterableIterator<any>) => number | undefined {
+    return (source: IterableIterator<any>) => {
         let result: number | undefined = undefined;
+        mapper = mapper || (item => item as number);
 
         for (const item of source) {
             result = Math.min(result === undefined ? Number.MAX_VALUE : result, mapper(item));
@@ -10,9 +15,14 @@ export function min<T>(mapper: (item: T) => number): (source: IterableIterator<T
     };
 }
 
-export function max<T>(mapper: (item: T) => number): (source: IterableIterator<T>) => number | undefined {
-    return (source: IterableIterator<T>) => {
+export function max<T>(mapper: (item: T) => number): (source: IterableIterator<T>) => number | undefined;
+
+export function max(): (source: IterableIterator<number>) => number | undefined;
+
+export function max(mapper?: (item: any) => number): (source: IterableIterator<any>) => number | undefined {
+    return (source: IterableIterator<any>) => {
         let result: number | undefined = undefined;
+        mapper = mapper ||  (item => item as number);
 
         for (const item of source) {
             result = Math.max(result === undefined ? Number.MIN_VALUE : result, mapper(item));
@@ -22,9 +32,14 @@ export function max<T>(mapper: (item: T) => number): (source: IterableIterator<T
     };
 }
 
-export function sum<T>(mapper: (item: T) => number): (source: IterableIterator<T>) => number {
-    return (source: IterableIterator<T>) => {
+export function sum<T>(mapper: (item: T) => number): (source: IterableIterator<T>) => number;
+
+export function sum(): (source: IterableIterator<number>) => number;
+
+export function sum(mapper?: (item: any) => number): (source: IterableIterator<any>) => number {
+    return (source: IterableIterator<any>) => {
         let result = 0;
+        mapper = mapper || (item => item as number);
 
         for (const item of source) {
             result += mapper(item);
--- tests/fp/math.spec.ts
+++ tests/fp/math.spec.ts
@@ -7,7 +7,7 @@ describe('fn/nav', () => {
 
     beforeEach(() => input = [5, 10, 3, 2, 0, 1, 7]);
 
-    it('min', () => {
+    it('min 1', () => {
         const result = pipe(
             core.iterate(input),
             math.min(x => x)
@@ -16,21 +16,37 @@ describe('fn/nav', () => {
         expect(result).toEqual(0);
     });
 
-    it('max', () => {
+    it('min 2', () => {
+        const result = math.min()(core.iterate(input));
+        expect(result).toEqual(0);
+    });
+
+    it('max 1', () => {
         const result = pipe(
             core.iterate(input),
             math.max(x => x)
         );
-
+        
         expect(result).toEqual(10);
     });
 
-    it('sum', () => {
+    it('max 2', () => {
+        const result = math.max()(core.iterate(input));
+        expect(result).toEqual(10);
+    });
+
+    it('sum 1', () => {
         const result = pipe(
             core.iterate(input),
             math.sum(x => x)
         );
 
+        const cmp = input.reduce((acc, x) => acc + x, 0);
+        expect(result).toEqual(cmp);
+    });
+
+    it('sum 2', () => {
+        const result = math.sum()(core.iterate(input));
         const cmp = input.reduce((acc, x) => acc + x, 0);
         expect(result).toEqual(cmp);
     });
